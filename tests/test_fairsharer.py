643e2b83c1456071bd8be9d65490959e0739b614
Yamanki96 <yaman.kisanieh@study.hs-Duesseldorf.de>
2026-02-04 20:55:08 +0100

    Add tests for fair_sharer function

--- tests/test_fairsharer.py
+++ tests/test_fairsharer.py
@@ -1,1 +1,10 @@
+from src.fairsharer import fair_sharer
+
+
+def test_example_one_iteration():
+    assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
+
+
+def test_example_two_iterations():
+    assert fair_sharer([0, 1000, 800, 0], 2) == [100, 890, 720, 90]
 
